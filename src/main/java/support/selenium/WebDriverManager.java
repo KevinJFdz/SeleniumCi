3bc80d8ab96c6219bee179d5e64fd85e52c2d541
Kevin <kevintfdz@endtest-mail.io>
2023-01-01 17:54:24 -0600

    Headless

--- src/main/java/support/selenium/WebDriverManager.java
+++ src/main/java/support/selenium/WebDriverManager.java
@@ -2,6 +2,7 @@ package support.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -11,7 +12,9 @@ public class WebDriverManager {
 
     public static WebDriver getChromeDriver(){
         io.github.bonigarcia.wdm.WebDriverManager.getInstance(CHROME).setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
