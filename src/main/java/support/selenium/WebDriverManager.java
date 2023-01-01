e35fcb199adf05638cb223d1ad9f135d9fd06c11
Kevin <kevintfdz@endtest-mail.io>
2023-01-01 14:30:28 -0600

    Updated WebDriver

--- src/main/java/support/selenium/WebDriverManager.java
+++ src/main/java/support/selenium/WebDriverManager.java
@@ -5,9 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+import static io.github.bonigarcia.wdm.config.DriverManagerType.CHROME;
+
 public class WebDriverManager {
 
     public static WebDriver getChromeDriver(){
+        io.github.bonigarcia.wdm.WebDriverManager.getInstance(CHROME).setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
