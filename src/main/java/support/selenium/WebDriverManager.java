8dc779bc96685808c49f89df33be1a9308aa83e0
Kevin <kevintfdz@endtest-mail.io>
2023-01-01 18:05:28 -0600

    Webdriver updated

--- src/main/java/support/selenium/WebDriverManager.java
+++ src/main/java/support/selenium/WebDriverManager.java
@@ -3,7 +3,10 @@ package support.selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import static io.github.bonigarcia.wdm.config.DriverManagerType.CHROME;
@@ -11,10 +14,14 @@ import static io.github.bonigarcia.wdm.config.DriverManagerType.CHROME;
 public class WebDriverManager {
 
     public static WebDriver getChromeDriver(){
-        io.github.bonigarcia.wdm.WebDriverManager.getInstance(CHROME).setup();
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        WebDriver driver = new ChromeDriver(chromeOptions);
+        WebDriver driver = null;
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),
+                    chromeOptions);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
